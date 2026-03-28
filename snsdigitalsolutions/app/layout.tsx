3621bbc3dfd28942f4a3dc8a8dac6ea92c673369
darshan sagare <87358897+darshan-111@users.noreply.github.com>
2026-03-28 16:37:52 +0530

    added icons to metadata

--- snsdigitalsolutions/app/layout.tsx
+++ snsdigitalsolutions/app/layout.tsx
@@ -13,6 +13,9 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
+  icons: {
+     icon: "/icon.png",
+  },
   title: "SNS Digital Solutions | Web Design & Development",
   description: "Premium web design and development services for modern businesses. We create stunning, responsive websites that convert.",
 };
